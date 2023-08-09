b1239e18c657868ebe4eca1ffb3ec01cf0b45fa3
seheerst <seherst9@gmail.com>
2023-08-09 23:16:24 +0300

    feat: added annotations

--- Models/User.cs
+++ Models/User.cs
@@ -1,11 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MeetingApp.Models
 {
     public class UserInfo
     {
+        [Required]
+        public int Id { get; set; }
+        [Required]
         public string? Name { get; set; }
+        [Required]
         public string? Phone { get; set; }
+        [Required]
+        [EmailAddress]
         public string? Email { get; set; }
-
+        
+        [Required]
         public bool? WillAttend { get; set; }
 
 
